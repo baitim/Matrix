e0646561a5e82ea05215b1a347a3dbdefc476c94
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-09-08 06:19:43 +0300

    added raii(rule of 5 with SRP), also update det
    determinant copies to double matrix, and then calculates

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,173 +1,239 @@
 #pragma once
 
+#include <cstring>
 #include "ANSI_colors.hpp"
 
 namespace matrix {
-template <typename ElemT> class matrix_buf_t {
-protected:
-    int rows_;
-    int cols_;
-    ElemT* elems_;
+    template <typename ElemT> class matrix_buf_t {
+    protected:
+        int rows_;
+        int cols_;
+        ElemT* elems_;
 
-    matrix_buf_t(int rows, int cols) : rows_(rows), cols_(cols) {
-        elems_ = new ElemT[rows_ * cols_];
-    }
-
-    ~matrix_buf_t() {
-        delete [] elems_;
-    }
-};
-
-template <typename ElemT> class matrix_t final : private matrix_buf_t<ElemT> {
-    using matrix_buf_t<ElemT>::rows_;
-    using matrix_buf_t<ElemT>::cols_;
-    using matrix_buf_t<ElemT>::elems_;
-
-    struct ProxyRow final {
-        ElemT* row;
-
-        ElemT& operator[](int n) {
-            return row[n];
+        matrix_buf_t(int rows, int cols) : rows_(rows), cols_(cols) {
+            elems_ = new ElemT[rows_ * cols_];
         }
 
-        ElemT operator[](int n) const {
-            return row[n];
+        matrix_buf_t(const matrix_buf_t<ElemT>& other) : matrix_buf_t<ElemT>(other.rows_, other.cols_) {
+            memcpy(elems_, other.elems_, sizeof(ElemT) * other.rows_ * other.cols_);
+        }
+
+        matrix_buf_t& operator=(const matrix_buf_t<ElemT>& other) {
+            if (this == &other)
+                return *this;
+
+            matrix_buf_t<ElemT> new_matrix_buf{other};
+            std::swap(rows_,  new_matrix_buf.rows_);
+            std::swap(cols_,  new_matrix_buf.cols_);
+            std::swap(elems_, new_matrix_buf.elems_);
+            return *this;
+        }
+
+        matrix_buf_t(matrix_buf_t<ElemT>&& other) noexcept : rows_(other.rows_), cols_(other.cols_), 
+                                                            elems_(other.elems_) {
+            other.rows_ = other.cols_ = 0;
+            other.elems_ = nullptr;
+        }
+        
+        matrix_buf_t& operator=(matrix_buf_t<ElemT>&& other) noexcept {
+            if (this == &other)
+                return *this;
+
+            std::swap(rows_,  other.rows_);
+            std::swap(cols_,  other.cols_);
+            std::swap(elems_, other.elems_);
+            return *this;
+        }
+
+        ~matrix_buf_t() {
+            delete [] elems_;
         }
     };
 
-public:
-    matrix_t(int rows, int cols) : matrix_buf_t<ElemT>(rows, cols) {}
+    template <typename ElemT> class matrix_t final : private matrix_buf_t<ElemT> {
+        using matrix_buf_t<ElemT>::rows_;
+        using matrix_buf_t<ElemT>::cols_;
+        using matrix_buf_t<ElemT>::elems_;
 
-    matrix_t(int rows, int cols, ElemT val) : matrix_t<ElemT>(rows, cols) {
-        for (int i = 0, end = rows_ * cols_; i < end; ++i)
-            elems_[i] = val;
-    }
+        struct ProxyRow final {
+            ElemT* row;
 
-    template <typename It>
-    matrix_t(int rows, int cols, It start, It fin) : matrix_t<ElemT>(rows, cols, 0) {
-        int i = 0;
-        int end = rows * cols;
-        for (It it = start; it < fin && i < end; ++it, ++i)
-            elems_[i] = *it;
-    }
+            ElemT& operator[](int n) {
+                return row[n];
+            }
 
-    static matrix_t<ElemT>* eye(int rows, int cols) {
-        matrix_t<ElemT>* matrix = new matrix_t<ElemT>(rows, cols, 0);
-        for (int i = 0, end = std::min(rows, cols); i < end; ++i)
-            matrix->elems_[i * cols + i] = 1;
-        return matrix;
-    }
+            ElemT operator[](int n) const {
+                return row[n];
+            }
+        };
 
-    ProxyRow& operator[](int n) {
-        return ProxyRow{elems_ + n * cols_};
-    }
+    public:
+        matrix_t(int rows, int cols) : matrix_buf_t<ElemT>(rows, cols) {}
 
-    ProxyRow operator[](int n) const {
-        return ProxyRow{elems_ + n * cols_};
-    }
+        matrix_t(int rows, int cols, ElemT val) : matrix_t<ElemT>(rows, cols) {
+            for (int i = 0, end = rows_ * cols_; i < end; ++i)
+                elems_[i] = val;
+        }
 
-    int get_rows() const noexcept {
-        return rows_;
-    }
+        template <typename It>
+        matrix_t(int rows, int cols, It start, It fin) : matrix_t<ElemT>(rows, cols, 0) {
+            int i = 0;
+            int end = rows * cols;
+            for (It it = start; it < fin && i < end; ++it, ++i)
+                elems_[i] = *it;
+        }
 
-    int get_cols() const noexcept {
-        return cols_;
-    }
+        static matrix_t<ElemT>* eye(int rows, int cols) {
+            matrix_t<ElemT>* matrix = new matrix_t<ElemT>(rows, cols, 0);
+            for (int i = 0, end = std::min(rows, cols); i < end; ++i)
+                matrix->elems_[i * cols + i] = 1;
+            return matrix;
+        }
 
-    ElemT trace() const noexcept {
-        ElemT trace = 0;
-        for (int i = 0, end = std::min(rows_, cols_); i < end; ++i)
-            trace += elems_[i * cols_ + i];
-        return trace;
-    }
+        ProxyRow& operator[](int n) {
+            return ProxyRow{elems_ + n * cols_};
+        }
 
-    bool swap_rows(int a, int b) noexcept {
+        ProxyRow operator[](int n) const {
+            return ProxyRow{elems_ + n * cols_};
+        }
+
+        int get_rows() const noexcept {
+            return rows_;
+        }
+
+        int get_cols() const noexcept {
+            return cols_;
+        }
+
+        ElemT trace() const noexcept {
+            ElemT trace = 0;
+            for (int i = 0, end = std::min(rows_, cols_); i < end; ++i)
+                trace += elems_[i * cols_ + i];
+            return trace;
+        }
+
+        bool   swap_rows    (matrix_t<double>& calc_matrix, int a, int b) noexcept;
+        void   simplify_rows(matrix_t<double>& calc_matrix, int i);
+        double diag_mult    (const matrix_t<double>& calc_matrix) const noexcept;
+
+        double determinant() {
+            if (cols_ != rows_)
+                return 0;
+
+            matrix_t<double> calc_matrix{*this};
+
+            bool is_swapped = false;
+            for (int i = 0; i < cols_; ++i) {
+
+                int pivot = i;
+                for (int j = i + 1; j < rows_; ++j) {
+                    int row_shift_j = j * cols_;
+                    if (abs(calc_matrix.elems_[row_shift_j + i])
+                        > abs(calc_matrix.elems_[row_shift_j + pivot]))
+                        pivot = j;
+                }
+
+                if (swap_rows(calc_matrix, i, pivot))
+                    is_swapped = !is_swapped;
+                
+                if (calc_matrix.elems_[i * cols_ + i] == 0)
+                    return 0;
+
+                simplify_rows(calc_matrix, i);
+            }
+
+            double det = diag_mult(calc_matrix);
+            if (is_swapped)
+                det *= -1;
+
+            return det;
+        }
+    };
+
+    template <typename ElemT>
+    bool matrix_t<ElemT>::swap_rows(matrix_t<double>& calc_matrix, int a, int b) noexcept {
         if (a == b)
             return false;
 
-        int row_shift_a = a * cols_;
-        int row_shift_b = b * cols_;
-        for (int i = 0; i < rows_; i++)
-            std::swap(elems_[row_shift_a + i], elems_[row_shift_b + i]);
+        int row_shift_a = a * calc_matrix.cols_;
+        int row_shift_b = b * calc_matrix.cols_;
+        for (int i = 0; i < calc_matrix.rows_; i++)
+            std::swap(calc_matrix.elems_[row_shift_a + i], calc_matrix.elems_[row_shift_b + i]);
 
         return true;
     }
 
-    void simplify_rows(int i) {
-        int row_shift_i = i * cols_;
-        for (int j = i + 1; j < rows_; ++j) {
+    template <typename ElemT>
+    void matrix_t<ElemT>::simplify_rows(matrix_t<double>& calc_matrix, int i) {
+        int row_shift_i = i * calc_matrix.cols_;
+        for (int j = i + 1; j < calc_matrix.rows_; ++j) {
 
-            int row_shift_j = j * cols_;
-            double coef = elems_[row_shift_j + i] / elems_[row_shift_i + i];
+            int row_shift_j = j * calc_matrix.cols_;
+            double coef = calc_matrix.elems_[row_shift_j + i] / calc_matrix.elems_[row_shift_i + i];
 
-            for (int k = 0; k < cols_; ++k)
-                elems_[row_shift_j + k] -= coef * elems_[row_shift_i + k];
+            for (int k = 0; k < calc_matrix.cols_; ++k)
+                calc_matrix.elems_[row_shift_j + k] -= coef * calc_matrix.elems_[row_shift_i + k];
         }
     }
 
-    double diag_mult() const noexcept {
+    template <typename ElemT>
+    double matrix_t<ElemT>::diag_mult(const matrix_t<double>& calc_matrix) const noexcept {
         double det = 1;
-        for (int i = 0, end = std::min(rows_, cols_); i < end; ++i)
-            det *= elems_[i * cols_ + i];
+        for (int i = 0, end = std::min(calc_matrix.rows_, calc_matrix.cols_); i < end; ++i)
+            det *= calc_matrix.elems_[i * calc_matrix.cols_ + i];
         return det;
     }
 
-    ElemT determinant() {
-        if (cols_ != rows_)
-            return 0;
+    template <typename ElemT>
+    void swap(matrix_t<ElemT>& x, matrix_t<ElemT>& y) {
+        matrix_t<ElemT> tmp = std::move(x);
+        x = std::move(y);
+        y = std::move(tmp);
+    }
 
-        bool is_swapped = false;
-        for (int i = 0; i < cols_; ++i) {
+    template <typename ElemT>
+    bool operator==(const matrix_t<ElemT>& x, const matrix_t<ElemT>& y) noexcept {
+        int xrows = x.get_rows();
+        int xcols = x.get_cols();
+        int yrows = y.get_rows();
+        int ycols = y.get_cols();
 
-            int pivot = i;
-            for (int j = i + 1; j < rows_; ++j) {
-                int row_shift_j = j * cols_;
-                if (abs(elems_[row_shift_j + i]) > abs(elems_[row_shift_j + pivot]))
-                    pivot = j;
+        if ((xrows != yrows) || (xcols != ycols))
+            return false;
+
+        for (int i = 0; i < xrows; i++)
+            for (int j = 0; j < xcols; j++)
+                if (x[i][j] != y[i][j])
+                    return false;
+
+        return true;
+    }
+
+    template <typename ElemT>
+    std::istream& operator>>(std::istream& is, const matrix_t<ElemT>& matrix) noexcept {
+        int rows = matrix.get_rows();
+        int cols = matrix.get_cols();
+
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                is >> matrix[i][j];
+        return is;
+    }
+
+    template <typename ElemT>
+    std::ostream& operator<<(std::ostream& os, const matrix_t<ElemT>& matrix) noexcept {
+        int rows = matrix.get_rows();
+        int cols = matrix.get_cols();
+
+        os << print_lblue("\nMatrix:\n");
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                os << print_lcyan(matrix[i][j]) << " ";
             }
-
-            if (swap_rows(i, pivot))
-                is_swapped = !is_swapped;
-            
-            if (elems_[i * cols_ + i] == 0)
-                return 0;
-
-            simplify_rows(i);
-        }
-
-        double det = diag_mult();
-        if (is_swapped)
-            det *= -1;
-
-        return det;
-    }
-};
-
-template <typename ElemT>
-std::istream& operator>>(std::istream& is, const matrix_t<ElemT>& matrix) noexcept {
-    int rows = matrix.get_rows();
-    int cols = matrix.get_cols();
-
-    for (int i = 0; i < rows; ++i)
-        for (int j = 0; j < cols; ++j)
-            is >> matrix[i][j];
-    return is;
-}
-
-template <typename ElemT>
-std::ostream& operator<<(std::ostream& os, const matrix_t<ElemT>& matrix) noexcept {
-    int rows = matrix.get_rows();
-    int cols = matrix.get_cols();
-
-    os << print_lblue("\nMatrix:\n");
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            os << print_lcyan(matrix[i][j]) << " ";
+            os << "\n";
         }
         os << "\n";
+        return os;
     }
-    os << "\n";
-    return os;
-}
-
 };
--- tests/matrix_unit_test.cpp
+++ tests/matrix_unit_test.cpp
@@ -37,18 +37,6 @@ TEST(Matrix_shuffle, end_to_end)
     globfree(&ans_files);
 }
 
-TEST(Matrix_main, test_double)
-{
-    int n = 3;
-    std::vector<double> elems{1.6, 1.9, 0.5,
-                              1.1, 1.3, 0.8,
-                              0.3, 0.4, 0.5};
-
-    matrix::matrix_t<double> matrix{n, n, elems.begin(), elems.end()};
-
-    ASSERT_LT(abs(matrix.determinant() - (-0.036)), EPSILON);
-}
-
 TEST(Matrix_main, test_fill_val)
 {
     int n = 3;
@@ -80,7 +68,19 @@ TEST(Matrix_main, test_eye)
     delete matrix;
 }
 
-TEST(Matrix_main, test_rectangle)
+TEST(Matrix_det, test_double)
+{
+    int n = 3;
+    std::vector<double> elems{1.6, 1.9, 0.5,
+                              1.1, 1.3, 0.8,
+                              0.3, 0.4, 0.5};
+
+    matrix::matrix_t<double> matrix{n, n, elems.begin(), elems.end()};
+
+    ASSERT_LT(abs(matrix.determinant() - (-0.036)), EPSILON);
+}
+
+TEST(Matrix_det, test_rectangle)
 {
     int n = 2, m = 3;
     std::vector<double> elems{1.3, 1.2, 0.7,
@@ -89,4 +89,55 @@ TEST(Matrix_main, test_rectangle)
     matrix::matrix_t<double> matrix{n, m, elems.begin(), elems.end()};
 
     ASSERT_LT(abs(matrix.determinant() - 0), EPSILON);
+}
+
+TEST(Matrix_det, test_det_cleanness)
+{
+    int n = 3;
+    std::vector<double> elems{1.4, 2.1, 0.5,
+                              1.6, 0.8, 1.7,
+                              0.9, 1.2, 1.8};
+
+    matrix::matrix_t<double> matrix{n, n, elems.begin(), elems.end()};
+    matrix::matrix_t<double> matrix2 = matrix;
+
+    ASSERT_LT(abs(matrix.determinant() - (-3.075)), EPSILON);
+    ASSERT_EQ((matrix == matrix2), 1);
+}
+
+TEST(Matrix_raii, test_copy_assign_ctor)
+{
+    int n = 2;
+    double default_value = 52;
+    matrix::matrix_t<double> matrix{n, n, default_value};
+
+    if (1) {
+        n = 3;
+        std::vector<double> elems{1.4, 2.1, 0.5,
+                                  1.6, 0.8, 1.7,
+                                  0.9, 1.2, 1.8};
+
+        matrix::matrix_t<double> matrix2{n, n, elems.begin(), elems.end()};
+        matrix = matrix2;
+    }
+
+    ASSERT_LT(abs(matrix.determinant() - (-3.075)), EPSILON);
+}
+
+TEST(Matrix_raii, test_move_assign_ctor)
+{
+    int n1 = 2;
+    double default_value = 52;
+    matrix::matrix_t<double> matrix1{n1, n1, default_value};
+    std::vector<double> elems{1.4, 2.1, 0.5,
+                              1.6, 0.8, 1.7,
+                              0.9, 1.2, 1.8};
+
+    int n2 = 3;
+    matrix::matrix_t<double> matrix2{n2, n2, elems.begin(), elems.end()};
+
+    matrix::swap(matrix1, matrix2);
+
+    ASSERT_LT(abs(matrix1.determinant() - (-3.075)), EPSILON);
+    ASSERT_LT(abs(matrix2.determinant() - 0), EPSILON);
 }
