7ec4b4b4b90865f1accbf7dd4a01e0a20351f0b0
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2025-02-05 04:54:18 +0300

    add deploy conan package

---
+++ conanfile.py
@@ -0,0 +1,49 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class matrixRecipe(ConanFile):
+    name = "matrix"
+    version = "1.0"
+
+    license = ""
+    author = ""
+    url = ""
+    description = "matrix"
+    topics = ""
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "tests/*"
+
+    def requirements(self):
+        self.requires("gtest/1.15.0")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["matrix"]
